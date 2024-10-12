4b1c9de7f6a79eef878168ec87bbbadb8e33c3ce
dipo <bazinoom@gmail.com>
2024-10-12 11:17:57 +0330

    Add infobox

---
+++ server.lua
@@ -0,0 +1,18 @@
+
+
+
+function addBox(sourceElement, type, msg)
+    if sourceElement.type ~= "player" then
+        if isElement(sourceElement:getData("inventory.open2")) or isElement(sourceElement:getData("inventory.open")) then
+            sourceElement = sourceElement:getData("inventory.open2") or sourceElement:getData("inventory.open")
+        end
+    end
+    
+    triggerClientEvent(sourceElement, "addBox", sourceElement, type, msg)
+end
+addEvent("addBox", true)
+addEventHandler("addBox", root, addBox)
+
+addCommandHandler("b", function(source)
+	exports["play_infobox"]:addBox(source, "info", "Acidentalmente")
+end)
